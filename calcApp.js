bde52cd27e539a8c70eec51eeecb1866427ee052
Jared Russell <jaredhrussell2016@gmail.com>
2023-05-22 12:35:40 -0400

    Modify calculation to accept sequential operands and minor css adjustments

--- calcApp.js
+++ calcApp.js
@@ -29,19 +29,50 @@ for(const btns of btnSelectr){
     })
 }
 
+const operatorSelector = document.querySelectorAll(".operator");
+for(const operatorBtn of operatorSelector){
+    operatorBtn.addEventListener('click', () => {
+        if(txtBx.value.includes(" + ") || txtBx.value.includes(" - ") ||
+        txtBx.value.includes(" / ") || txtBx.value.includes(" x ")){
+            let input = txtBx.value;
+            const newInput = input.split(" ");
+            let operand1 = Number(newInput[0]);
+            let operand2 = Number(newInput[2]);
+            let operator = newInput[1];
+            txtBx.value = operate(operand1, operand2, operator);
+            txtBx.value += operatorBtn.textContent;
+        }
+        else {
+            txtBx.value += operatorBtn.textContent;
+        }
+    })
+}
+
+
 //clear button
 const clear_button = document.querySelector(".btnClear");
 clear_button.addEventListener('click', () =>{
     txtBx.value = '';
 })
 
-
-const enter_button = document.querySelector(".btnEnter");
-enter_button.addEventListener('click', () => {
-    let input = txtBx.value;
-    const newInput = input.split(" ");
-    let oeprand1 = Number(newInput[0]);
-    let operand2 = Number(newInput[2]);
-    let operator = newInput[1];
-    txtBx.value = operate(oeprand1, operand2, operator);
+//delete button
+const delete_button = document.querySelector(".btnDelete");
+delete_button.addEventListener('click', () => {
+    txtBx.value = txtBx.value.slice(0, txtBx.value.length - 1);
 })
+
+
+const enter_button = document.querySelector(".equal");
+enter_button.addEventListener('click', () => {
+    if (txtBx.value.length >= 5){
+        let input = txtBx.value;
+        const newInput = input.split(" ");
+        let operand1 = Number(newInput[0]);
+        let operand2 = Number(newInput[2]);
+        let operator = newInput[1];
+        txtBx.value = operate(operand1, operand2, operator);
+    }
+})
+
+
+
